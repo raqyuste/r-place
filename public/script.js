6d84a27b0d2f3931d8336e1e12018676451f644c
Raquel Yuste <raquel@tinybird.co>
2022-06-02 16:16:16 +0200

    post pixel position on datasource

--- public/script.js
+++ public/script.js
@@ -33,36 +33,53 @@ const init = function () {
   canvas.height = CANVAS_HEIGHT;
   canvas.style.width = `${CANVAS_WIDTH}px`;
   canvas.style.height = `${CANVAS_HEIGHT}px`;
+};
 
+window.onmousedown = function (event) {
+  getMousePos(canvas, event);
   draw();
 };
-
-window.onmousedown = function () {
-    draw()
-};
-
 
 colorpicker.oninput = function () {
   color = this.value;
 };
 
-
 function getMousePos(canvas, event) {
   let rect = canvas.getBoundingClientRect();
 
-  currentX = Math.floor((event.clientX - rect.left));
-  currentY = Math.floor((event.clientY - rect.top));
+  currentX = Math.floor(event.clientX - rect.left);
+  currentY = Math.floor(event.clientY - rect.top);
+}
+
+function now() {
+  const d = new Date();
+  const result = `${d.toISOString().slice(0, 10)} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+  return result;
 }
 
 function draw() {
-    ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.fillRect(currentX, currentY, 1, 1);
-    ctx.fill();
-}
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.fillRect(currentX, currentY, 1, 1);
+  ctx.fill();
 
-window.onmousemove = function (event) {
-  getMousePos(canvas, event);
-};
+  const row = {
+    x: currentX,
+    y: currentY,
+    timestamp: now(),
+    color: color,
+  };
+
+  fetch("https://api.tinybird.co/v0/events?name=pixels_table", {
+    method: "post",
+    headers: new Headers({
+      Authorization:
+        "Bearer p.eyJ1IjogIjM0YmRiNTJkLTRiYjYtNDljZi04ZjdjLWI4MmM3MjVmNjRmNSIsICJpZCI6ICJiYzYwZjYzOC1lYzAwLTQxYTgtODhkNS05ZmNhZmNhNmI0MDUifQ.BBKGRtAlvq_cFP-anEaaYi6WSViUWQuVAvB_kSY4qig",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    }),
+    body: JSON.stringify(row),
+  });
+}
 
 init();
