d35e509945c36749a72be42276923644309c1973
Raquel Yuste <raquel@tinybird.co>
2022-06-02 16:49:51 +0200

    not request again if the previous request hasnt finished

--- public/script.js
+++ public/script.js
@@ -2,19 +2,10 @@ const colorpicker = document.getElementById("colorpicker");
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
-const tempCanvas = document.createElement("canvas");
-const tempCtx = tempCanvas.getContext("2d");
-
 CANVAS_WIDTH = 1000;
 CANVAS_HEIGHT = 500;
 
-let currentWidth = CANVAS_WIDTH;
-let currentHeight = CANVAS_HEIGHT;
-
 let currentColor = colorpicker.value;
-
-let currentX = 0;
-let currentY = 0;
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
@@ -24,14 +15,14 @@ canvas.style.height = `${CANVAS_HEIGHT}px`;
 ctx.mozImageSmoothingEnabled = false;
 ctx.imageSmoothingEnabled = false;
 
-const init = function () {
-  currentWidth = CANVAS_WIDTH;
-  currentHeight = CANVAS_HEIGHT;
-  canvas.width = CANVAS_WIDTH;
-  canvas.height = CANVAS_HEIGHT;
-  canvas.style.width = `${CANVAS_WIDTH}px`;
-  canvas.style.height = `${CANVAS_HEIGHT}px`;
+function draw({ x, y, color }) {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, 1, 1);
+  ctx.fill();
+}
 
+const init = function () {
   fetch(
     "https://api.tinybird.co/v0/pipes/get_snapshot.json?token=p.eyJ1IjogIjM0YmRiNTJkLTRiYjYtNDljZi04ZjdjLWI4MmM3MjVmNjRmNSIsICJpZCI6ICJiYzYwZjYzOC1lYzAwLTQxYTgtODhkNS05ZmNhZmNhNmI0MDUifQ.BBKGRtAlvq_cFP-anEaaYi6WSViUWQuVAvB_kSY4qig",
     {
@@ -42,21 +33,10 @@ const init = function () {
       return response.json();
     })
     .then(function ({ data }) {
-      data.forEach(function ({ x, y, color }) {
-        ctx.fillStyle = color;
-        ctx.fillRect(x, y, 1, 1);
+      data.forEach(function (item) {
+        draw(item);
       });
     });
-};
-
-canvas.onmousedown = function (event) {
-  const pixel = getMousePos(canvas, event);
-  draw(pixel);
-  ingestNewPixel(pixel);
-};
-
-colorpicker.oninput = function () {
-  currentColor = this.value;
 };
 
 function getMousePos(canvas, event) {
@@ -69,6 +49,16 @@ function getMousePos(canvas, event) {
   };
 }
 
+canvas.onmousedown = function (event) {
+  const pixel = getMousePos(canvas, event);
+  draw(pixel);
+  ingestNewPixel(pixel);
+};
+
+colorpicker.oninput = function () {
+  currentColor = this.value;
+};
+
 function now(secondsBefore = 0) {
   const d = new Date();
   const seconds = parseInt(d.getSeconds()) - secondsBefore;
@@ -77,7 +67,6 @@ function now(secondsBefore = 0) {
     .slice(0, 10)} ${d.getHours()}:${d.getMinutes()}:${
     seconds < 10 ? `0${seconds}` : seconds
   }`;
-  console.log(result);
   return result;
 }
 function ingestNewPixel(pixel) {
@@ -98,24 +87,23 @@ function ingestNewPixel(pixel) {
   });
 }
 
-function draw(pixel) {
-  ctx.beginPath();
-  ctx.fillStyle = pixel.color;
-  ctx.fillRect(pixel.x, pixel.y, 1, 1);
-  ctx.fill();
-}
-
 init();
 
+let isRequesting = false;
+
 window.setInterval(function () {
-  fetch(
-    `https://api.tinybird.co/v0/pipes/get_snapshot.json?token=p.eyJ1IjogIjM0YmRiNTJkLTRiYjYtNDljZi04ZjdjLWI4MmM3MjVmNjRmNSIsICJpZCI6ICJiYzYwZjYzOC1lYzAwLTQxYTgtODhkNS05ZmNhZmNhNmI0MDUifQ.BBKGRtAlvq_cFP-anEaaYi6WSViUWQuVAvB_kSY4qig&date_start=${now(
-      10
-    )}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const array = data.data;
-      array.map((item) => draw(item));
-    });
+  if (!isRequesting) {
+    fetch(
+      `https://api.tinybird.co/v0/pipes/get_snapshot.json?token=p.eyJ1IjogIjM0YmRiNTJkLTRiYjYtNDljZi04ZjdjLWI4MmM3MjVmNjRmNSIsICJpZCI6ICJiYzYwZjYzOC1lYzAwLTQxYTgtODhkNS05ZmNhZmNhNmI0MDUifQ.BBKGRtAlvq_cFP-anEaaYi6WSViUWQuVAvB_kSY4qig&date_start=${now(
+        10
+      )}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        isRequesting = false;
+        const array = data.data;
+        array.forEach((item) => draw(item));
+      });
+  }
+  isRequesting = true;
 }, 1000);
